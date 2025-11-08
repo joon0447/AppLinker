222248253de6aa3169b7ed6ffc99406d1beb877f
joon0447 <joon0447@naver.com>
2025-11-09 00:28:45 +0900

    feat(command) : 코드 발급 함수 추가

--- src/main/kotlin/org/joon/appLinker/Command/LinkCommand.kt
+++ src/main/kotlin/org/joon/appLinker/Command/LinkCommand.kt
@@ -4,6 +4,7 @@ import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
+import org.joon.appLinker.Constant.Message
 
 class LinkCommand : CommandExecutor {
     override fun onCommand(
@@ -13,5 +14,19 @@ class LinkCommand : CommandExecutor {
         args: Array<out String>
     ): Boolean {
         if(sender !is Player) return false
+        val code = generateCode(CODE_LENGTH)
+        sender.sendMessage(Message.codeMessage(code))
+        return true
+    }
+
+    private fun generateCode(length: Int): String {
+        val chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+        return (1..length)
+            .map { chars.random() }
+            .joinToString("")
+    }
+
+    companion object {
+        const val CODE_LENGTH = 6
     }
 }
