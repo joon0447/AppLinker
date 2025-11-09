3a539b4276c2cc7798aca48d68edb403452d34d7
joon0447 <joon0447@naver.com>
2025-11-09 11:37:56 +0900

    feat(main, command) : 파이어베이스 초기화 , 데이터 폴더 생성 확인 추가

--- src/main/kotlin/org/joon/appLinker/AppLinker.kt
+++ src/main/kotlin/org/joon/appLinker/AppLinker.kt
@@ -2,20 +2,29 @@ package org.joon.appLinker
 
 import org.bukkit.plugin.java.JavaPlugin
 import org.joon.appLinker.Command.LinkCommand
+import org.joon.appLinker.Util.DataFolder
+import org.joon.appLinker.Util.Firebase
 
 class AppLinker : JavaPlugin() {
 
     override fun onEnable() {
-        // Plugin startup logic
+        plugin = this
+        DataFolder.checkDataFolderExists()
+        Firebase.initFirebase()
         loadCommands()
         println("AppLinker enabled")
     }
 
     override fun onDisable() {
-        // Plugin shutdown logic
+
     }
 
     private fun loadCommands() {
-        getCommand("앱")?.setExecutor(LinkCommand())
+        getCommand("앱")?.setExecutor(LinkCommand(Firebase.getFirestore()))
+    }
+
+    companion object {
+        lateinit var plugin : AppLinker
+            private set
     }
 }
--- src/main/kotlin/org/joon/appLinker/Command/LinkCommand.kt
+++ src/main/kotlin/org/joon/appLinker/Command/LinkCommand.kt
@@ -1,12 +1,13 @@
 package org.joon.appLinker.Command
 
+import com.google.cloud.firestore.Firestore
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
 import org.joon.appLinker.Constant.Message
 
-class LinkCommand : CommandExecutor {
+class LinkCommand(firestore: Firestore) : CommandExecutor {
     override fun onCommand(
         sender: CommandSender,
         command: Command,
